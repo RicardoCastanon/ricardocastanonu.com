41586725446a763ee46fc25dd5156daa541be74d
Brandon Smith <boombyeyt@gmail.com>
2022-09-18 11:09:34 -0500

    try prod

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -3,7 +3,7 @@ import ProjectCard from "./ProjectCard";
 
 function Projects() {
   return (
-    <div className="w-full">
+    <div id="projects" className="w-full">
       <div className="max-w-[1240px] mx-auto px-2 py-16">
         <p className="text-xl tracking-widest uppercase text-[#5651e5]">
           Projects
