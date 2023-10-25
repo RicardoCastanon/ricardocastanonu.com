420b41333b83923bb3e40f5b77906aa7c7205bb8
Ricardo Castanon <ricardocastanonu@gmail.com>
2023-10-24 19:30:21 -0500

    Fields of Interest section completed.

--- components/FieldsOfInterest.jsx
+++ components/FieldsOfInterest.jsx
@@ -1,25 +1,113 @@
 import React from 'react';
 
 function FieldsOfInterest() {
-    return(
+    return (
         <div className="max-w-[1240px] mx-auto px-2 py-3">
             <h2 className="py-6">{"Fields that I would like to learn more about : "}</h2>
             <p>
-                Other than the various engineering disciplines that intrigue me—  
-                <i>computer, hardware, software, and electrical—</i>there are other fields 
-                I would like to learn more about, work in, and contribute to. Below are the 
+                Other than the various engineering disciplines that intrigue me—
+                <i>computer, hardware, software, and electrical—</i>there are other fields
+                I would like to learn more about, work in, and contribute to. Below are the
                 fields in which I wish to work in or explore further in the future as well
                 more specific topics and questions that engross me.
             </p>
 
             <h3 className="py-2 text-xl">{"Astronomy"}</h3>
+            <p>These are questions I would like to explore:</p>
+            <ul className="list-disc px-5">
+                <li>What happened before the Big Bang?</li>
+                <li>How are solar systems formed?</li>
+                <li>How are planets formed?</li>
+                <li>How rare is our Solar System when it comes to suitability for life?</li>
+                <li>What are the factors that affect suitability for life?</li>
+                <li>Are we alone? If not, where is everybody?</li>
+                <li>Can humanity achieve a Kardashev level 1?</li>
+                <li>How can we efficiently research outside solar systems and exoplanets?</li>
+                <li>How can we efficiently search for possible alien communication?</li>
+            </ul>
+            <h3>My game plan:</h3>
+            <ul className="list-disc px-5">
+                <li>
+                    Learn <strong>data science</strong> while advancing my knowledge on
+                    <strong> physics</strong> and <strong>mathematics</strong> to model the universe,
+                    solar systems, and planet formation to learn more about our universe's evolution.
+                </li>
+                <li>
+                    Use <strong>machine learning</strong> to help advance our understanding of physics
+                    to be able to better map the universe's evolution and behavior.
+                </li>
+                <li>
+                    Use <strong>data science</strong> to analyze collected data from interstellar
+                    space to learn about solar systems, exoplanets, and find possible habitable
+                    planets.
+                </li>
+                <li>
+                    Use <strong>data science</strong> and <strong>machine learning</strong> to
+                    more efficiently analyze potential messages being received from space.
+                </li>
+            </ul>
 
             <h3 className="py-2 text-xl">{"Neuroscience"}</h3>
+            <p>These are questions I would like to explore:</p>
+            <ul className="list-disc px-5">
+                <li>
+                    How can I apply neuroscience outside of the medical field and more related
+                    towards technology?
+                </li>
+                <li>How special is the human brain?</li>
+                <li>How can neuroscience help us understand consciousness?</li>
+                <li>How can neuroscience help differentiate ourselves from other animals?</li>
+                <li>
+                    What can we learn from comparing human neuroscience and machine
+                    <strong> neural networks</strong>?
+                </li>
+            </ul>
+            <h3>My game plan:</h3>
+            <ul className="list-disc px-5">
+                <li>
+                    Learn <strong>machine learning</strong>, <strong>neural networks</strong>,
+                    and <strong>neuroscience</strong> to research the similarities and
+                    differences of human evolution to the evolution of human technology.
+                </li>
+                <li>
+                    Create <strong>artificial intelligence</strong> that is safe for humanity
+                    to use, with the goal of using AI to benefit society.
+                </li>
+                <li>
+                    Better understand the universe as we learn more about our consciousness.
+                </li>
+            </ul>
 
             <h3 className="py-2 text-xl">{"Climate Sustainability"}</h3>
-
-
-
+            <p>These are questions I would like to explore:</p>
+            <ul className="list-disc px-5">
+                <li>
+                    What can we do better to take care of Earth?
+                </li>
+                <li>
+                    How can we improve the efficiency of renewable energy to lower
+                    emissions?
+                </li>
+                <li>
+                    What newly introduced technologies can actually help solve or 
+                    help suppress the climate crisis?
+                </li>
+            </ul>
+            <h3>My game plan:</h3>
+            <ul className="list-disc px-5">
+                <li>
+                    Learn about efficient energy practices and methods through the knowledge
+                    I gain in <strong>electrical engineering</strong>.
+                </li>
+                <li>
+                    Use my <strong>computer engineering</strong> expertise to invent or 
+                    improve products or methods in terms of sustainability.
+                </li>
+                <li>
+                    Advocate for corporations to implement green solutions in their
+                    engineering and manufacturing practices.
+                </li>
+            </ul>
         </div>
     );
 }
