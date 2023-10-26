744edf3befe7de7054e14b88588e380c83dd9178
Ricardo Castanon <ricardocastanonu@gmail.com>
2023-10-25 19:25:04 -0500

    Deleted Brandon Project Section

--- components/AdditionalSkills.jsx
+++ components/AdditionalSkills.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function AdditionalSkills() {
     return (
-        <div className="max-w-[1240px] mx-auto px-2 py-3 pt-32" id='additionalskills'>
+        <div className="max-w-[1240px] mx-auto px-2 py-3 pt-16" id='additionalskills'>
             <div>
                 <h2 className="py-6">{"Other Skills being Developed: "}</h2>
                 <ul className="list-disc px-7">
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,6 @@ import Navbar from "../components/Navbar";
 import Main from "../components/Main";
 import About from "../components/About";
 import Skills from "../components/Skills";
-import Projects from "../components/Projects";
 import Contact from "../components/Contact";
 import { MyResearch } from "../components/MyResearch";
 import { SuccessModal } from "../components/SuccessModal";
@@ -73,8 +72,7 @@ const Home: NextPage = () => {
       <Main />
       <About />
       <Skills />
-      <Projects />
-      {/* <MyResearch /> */}
+      <MyResearch />
       <AdditionalSkills />
       <FieldsOfInterest />
       <Contact onClick={getIsModalOpen} />
